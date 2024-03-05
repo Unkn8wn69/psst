74fdfebb57572335ebb1e8a2a6206e076b13bdc6
Unkn8wn69 <79060957+Unkn8wn69@users.noreply.github.com>
2024-03-05 23:09:23 +0100

    added management of the content. Depending on the chosen tab

--- gui.py
+++ gui.py
@@ -2,6 +2,9 @@ import tkinter as tk
 import customtkinter as ctk
 import webbrowser
 
+# Local Imports
+from about import create_about_page
+
 # Constants
 APP_TITLE = "PSST"
 APP_VERSION = "0.0.1"
@@ -17,18 +20,32 @@ BUTTON_FG = "#5A5A66"
 tabs = ["Create", "Recover", "About"]
 current_tab = "About"
 
-def on_button_click(tab):
+# Tab-Content Functions
+
+def clear_main_content():
+    for widget in main_content_frame.winfo_children():
+        widget.destroy()
+
+def update_main_content(tab):
+    clear_main_content()
+    if tab == "About":
+        create_about_page(main_content_frame)
+
+# Top bar functions
+
+def on_tab_button_click(tab):
     global current_tab
     current_tab = tab
     print(f"Tab changed to: {tab}")
     update_tab_buttons()
+    update_main_content(current_tab)
 
 def open_github():
     webbrowser.open(GITHUB_URL)
 
 def tab_buttons(top_bar):
     for tab in tabs:
-        button = ctk.CTkButton(top_bar, text=tab, command=lambda tab=tab: on_button_click(tab))
+        button = ctk.CTkButton(top_bar, text=tab, command=lambda tab=tab: on_tab_button_click(tab))
         button.pack(side="left", padx=5)
         button.tab_name = tab
         if tab == current_tab:
@@ -51,7 +68,7 @@ def create_top_bar(parent):
     top_bar.pack(side="top", fill="x")
 
     title_label = ctk.CTkLabel(top_bar, text="PSST", anchor="w")
-    title_label.pack(side="left", padx=(10, 0))
+    title_label.pack(side="left", padx=(10, 20))
 
     tab_buttons(top_bar)
 
@@ -66,11 +83,18 @@ def create_top_bar(parent):
 
     icon_label.image = small_icon
 
+# Start of the application
+
 app = ctk.CTk()
 app.title(APP_TITLE)
 app.geometry(APP_GEOMETRY)
 ctk.set_appearance_mode("dark")
 
 create_top_bar(app)
+
+main_content_frame = ctk.CTkFrame(app)
+main_content_frame.pack(fill="both", expand=True, padx=20, pady=20)
+
+update_main_content(current_tab)
 
 app.mainloop()
